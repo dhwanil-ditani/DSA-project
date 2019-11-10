76e4622b7827e9133206fbfdb681ec8675f1cbc5
dh711 <dhairya711@gmail.com>
2019-11-11 02:16:18 +0530

    fixed ∩, symDiff, made dsp better & code readable.

--- sets.c
+++ sets.c
@@ -1,178 +1,297 @@
-#include<stdio.h>
-#include<stdlib.h>
-struct ele
+#include <stdio.h>
+#include <stdlib.h>
+
+//Node structure for storing elements.
+struct node
 {
-    int val;
-    struct ele*next;
+    int value;
+    struct node *next;
 };
-struct ele*new;
-struct ele*temp;
-void createnode(int val)
+
+typedef struct node element;
+
+//Creates a node and returns it to the calling function.
+element* createNode(int value)
 {
-    new=(struct ele*)malloc(sizeof(struct ele));
-    new->next=NULL;
-    new->val=val;
+    element* new = (element*)malloc(sizeof(element));       //Allocating memory to pointer of type element.
+    new->value = value;
+    new->next = NULL;
+    return new;
 }
-struct ele*insord(struct ele*head)
-{
-    struct ele*temp1;
-    if(head==NULL)
-       head=new;
+
+//Inserts numbers into a set in ascending order.
+element* insertElements(element* head, int value)
+{    
+    if(head == NULL)
+       head = createNode(value);
+    
     else
     {
-        temp=head;
-        while(temp!=NULL)
+        element *new, *temp1, *temp2;
+        new = createNode(value);
+        temp1 = head;
+        
+        while (temp1 != NULL)
         {
-            if(new->val<=temp->val)
+            if (new->value < temp1->value)
                 break;
-            temp1=temp;
-            temp=temp->next;
+            
+            else if (new->value == temp1->value)
+            {
+                printf("\nElement already exists in set.\n");
+                goto l;    
+            }
+            
+            temp2 = temp1;
+            temp1 = temp1->next;
         }
-        if(temp==head)
-            head=new;
+        
+        if (temp1 == head)
+            head = new;
+        
         else
-            temp1->next=new;
-        if(temp!=NULL)
-            new->next=temp;
+            temp2->next = new;
+        
+        if (temp1 != NULL)
+            new->next = temp1;
     }
-    return head;
+    
+    l: return head;
 }
-struct ele*merge(struct ele*h,struct ele*h2)
+
+//Finds union of sets A & B.
+element* setUnion(element* set1, element* set2)
 {
-    struct ele*h3=NULL;
-    while(h!=NULL&&h2!=NULL)
+    element *set3 = NULL, *temp, *new;
+    
+    while (set1 != NULL && set2 != NULL)
     {
-        if(h->val < h2->val)
+        if (set1->value < set2->value)
         {
-            createnode(h->val);
-            h=h->next;
+            new = createNode(set1->value);
+            set1 = set1->next;
         }
-        else if(h2->val < h->val)
+        
+        else if (set2->value < set1->value)
         {
-            createnode(h2->val);
-            h2=h2->next;
+            new = createNode(set2->value);
+            set2 = set2->next;
         }
+        
         else
         {
-            createnode(h->val);
-            h=h->next;
-            h2=h2->next;
+            new = createNode(set1->value);
+            set1 = set1->next;
+            set2 = set2->next;
         }
-        if(h3==NULL)
-            temp=h3=new;
+        
+        if (set3 == NULL)
+            temp = set3 = new;
+        
         else
         {
-            temp->next=new;
-            temp=new;
+            temp->next = new;
+            temp = new;
         }
     }
-    if(h==NULL)
-        while(h2!=NULL)
+    
+    if (set1 == NULL)
+    {
+        while (set2 != NULL)
         {
-            createnode(h2->val);
-            temp->next=new;
-            temp=new;
-            h2=h2->next;
+            new = createNode(set2->value);
+            temp->next = new;
+            temp = new;
+            set2 = set2->next;
         }
+    }
+    
     else
-        while(h!=NULL)
+    {
+        while(set1 != NULL)
         {
-            createnode(h->val);
-            temp->next=new;
-            temp=new;
-            h=h->next;
+            new = createNode(set1->value);
+            temp->next = new;
+            temp = new;
+            set1 = set1->next;
         }
-    return h3;
+    }
+    
+    return set3;
 }
-struct ele* inter(struct ele* h,struct  ele* h2)
+
+//Finds intersection of sets A & B.
+element* setIntersection(element* set1, element* set2)
 {
-    struct ele* h3=NULL;
-    temp = NULL;
-    while(h!=NULL&&h2!=NULL)
+    element *set3 = NULL, *temp, *new;
+
+    while(set1 != NULL && set2 != NULL)
     {
         new = NULL;
-        if(h->val < h2->val)          
-            h=h->next;
-        else if(h2->val < h->val)            
-            h2=h2->next;
-        else
+        
+        if (set1->value < set2->value)          
+            set1 = set1->next;
+        
+        else if (set2->value < set1->value)            
+            set2 = set2->next;
+        
+        else if (set1->value == set2->value)
         {
-            createnode(h->val);
-            h=h->next;
-            h2=h2->next;
+            new = createNode(set1->value);
+            set1 = set1->next;
+            set2 = set2->next;
         }
-        if(h3==NULL)
-            temp=h3=new;
-        else
+        
+        if (set3 == NULL)
+            temp = set3 = new;
+        
+        else if (new != NULL)
         {
-            temp->next=new;
-            temp=new;
+            temp->next = new;
+            temp = new;
         }
     }
-    return h3;
+
+    return set3;
 }
-void display(struct ele*h)
+
+//Finds symmetric difference of sets A & B.
+element* setSymDiff(element* set1, element* set2)
 {
-    printf("THE ELEMENTS ARE:  ");
-    while(h->next!=NULL)
+    element *set3 = NULL, *temp = NULL, *new;
+    
+    while(set1 != NULL && set2 != NULL)
     {
-        printf("%d , ",h->val);
-        h=h->next;
+        new = NULL;
+        if (set1->value < set2->value)
+        {          
+            new = createNode(set1->value);
+            set1 = set1->next;
+        }
+        
+        else if (set2->value < set1->value)
+        {
+            new = createNode(set2->value);
+            set2 = set2->next;
+        }
+
+        else
+        {
+            set1 = set1->next;
+            set2 = set2->next;
+        }
+        
+        if (set3 == NULL)
+            temp = set3 = new;
+        
+        else if (new != NULL)
+        {
+            temp->next = new;
+            temp = new;
+        }
     }
-    printf("%d .",h->val);
+    
+    if (set1 == NULL)
+    {
+        while (set2 != NULL)
+        {
+            new = createNode(set2->value);
+            temp->next = new;
+            temp = new;
+            set2 = set2->next;
+        }
+    }
+    
+    else
+    {
+        while(set1 != NULL)
+        {
+            new = createNode(set1->value);
+            temp->next = new;
+            temp = new;
+            set1 = set1->next;
+        }
+    }
+
+    return set3;
 }
+
+//Displays a set.
+void setDisplay(element* set)
+{
+    printf("{");
+    while (set->next != NULL)
+    {
+        printf("%d, ", set->value);
+        set = set->next;
+    }
+
+    printf("%d}", set->value);
+}
+
 void main()
 {
-    struct ele*head=NULL;
-    struct ele*head2=NULL;
-    int val;
-    char ch;
-    printf("enter elements of set 1\nenter first element\n");
+    element* set1 = NULL;   //Set A.
+    element* set2 = NULL;   //Set B.
+    element* set3 = NULL;   //Set C, used according to requirement.
+    
+    int value;
+    char choice;
+    
+    //Inserting elements in set A.
+    printf("Enter elements of set A: ");
     do
     {
-        printf("element:");
-        scanf("%d",&val);
-        createnode(val);
-        head=insord(head);
-        printf("enter another element(y/n)");
-        scanf("%c",&ch);
-        scanf("%c",&ch);
-    }while(ch=='y'||ch=='Y');
-    printf("enter elements of set 2\nenter first element\n");
+        printf("\nElement: ");
+        scanf("%d", &value);
+        set1 = insertElements(set1, value);
+        printf("\nEnter another element? [Y/N]\n");
+        scanf("%c", &choice);
+        scanf("%c", &choice);
+    } while (choice == 'y' || choice == 'Y');
+    
+    printf("\nSet A = ");
+    setDisplay(set1);
+    
+    //Inserting elements in set A.
+    printf("\n\nEnter elements of set B: ");
     do
     {
-        printf("element:");
-        scanf("%d",&val);
-        createnode(val);
-        head2=insord(head2);
-        printf("enter another element(y/n)");
-        scanf("%c",&ch);
-        scanf("%c",&ch);
-    }while(ch=='y'||ch=='Y');
-    printf("THE TWO SETS ENTERED SUCCESSFULLY");
-    do
-    {
-       printf("\n\nmenu for operations\n1. UNION\n2. INTERSECTION\n3. DHAIRYA\n4. EXIT");
-       scanf("%d",&ch);
-       switch (ch)
-       {
-        case 1:
-           display(merge(head,head2));
-           break;
-        case 2:
-                if(inter(head,head2)== NULL)
-                    printf("no common elements...");
-                else
-                    display(inter(head,head2));
-                break;    
-        case 3:
-            printf("dhairya karega case 3");
-            break;
-        case 4:
-            printf("EXITING....");
-            break;       
-        default:
-           printf("wrong choice....enter again");
-       }
-    }while (ch!=4);
+        printf("\nElement: ");
+        scanf("%d", &value);
+        set2 = insertElements(set2, value);
+        printf("\nEnter another element? [Y/N]\n");
+        scanf("%c", &choice);
+        scanf("%c", &choice);
+    } while (choice == 'y' || choice == 'Y');
+    
+    printf("\nSet B = ");
+    setDisplay(set2);
+
+    printf("\n");
+
+    printf("\nA (union) B = ");
+    set3 = setUnion(set1, set2);
+    setDisplay(set3);
+    printf("\n");
+ 
+    printf("\nA (intersection) B = ");
+    set3 = setIntersection(set1, set2);
+    if (set3 == NULL)
+        printf("phi");
+                    
+    else
+       setDisplay(set3);
+                    
+    printf("\n");
+
+    printf("\nA (symmetric diff) B = ");
+    set3 = setSymDiff(set1, set2); 
+    if(set3 == NULL)
+        printf("phi");
+                    
+    else
+        setDisplay(set3);
+                    
+    printf("\n\n");
 }
