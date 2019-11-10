d47716d31d4f74aeaca908c34108cbb0a2c42c9c
DJGARG7 <57539004+DJGARG7@users.noreply.github.com>
2019-11-10 16:37:54 +0530

    Update sets.c

--- sets.c
+++ sets.c
@@ -1,99 +1,178 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-struct LinkedList {
-    int data;
-    struct LinkedList *next;
+#include<stdio.h>
+#include<stdlib.h>
+struct ele
+{
+    int val;
+    struct ele*next;
 };
-
-typedef struct LinkedList *Set;
-
-Set createNode() {
-    Set newNode = (Set)malloc(sizeof(struct LinkedList));
-    newNode->data = 0;
-    newNode->next = NULL;
-    return newNode;
+struct ele*new;
+struct ele*temp;
+void createnode(int val)
+{
+    new=(struct ele*)malloc(sizeof(struct ele));
+    new->next=NULL;
+    new->val=val;
 }
-
-void display(Set head) {
-    while(head != NULL) {
-        printf("%d\t", head->data);
-        printf("%d\n", head->next);
-        head = head->next;
+struct ele*insord(struct ele*head)
+{
+    struct ele*temp1;
+    if(head==NULL)
+       head=new;
+    else
+    {
+        temp=head;
+        while(temp!=NULL)
+        {
+            if(new->val<=temp->val)
+                break;
+            temp1=temp;
+            temp=temp->next;
+        }
+        if(temp==head)
+            head=new;
+        else
+            temp1->next=new;
+        if(temp!=NULL)
+            new->next=temp;
     }
-    printf("\n");
+    return head;
 }
-
-Set merge(Set s1, Set s2) {
-    Set s3 = NULL;
-    Set curr = NULL;
-    while(s1 != NULL || s2 != NULL) {
-        if(s3 == NULL) {
-            s3 = createNode();
-            curr = s3;
+struct ele*merge(struct ele*h,struct ele*h2)
+{
+    struct ele*h3=NULL;
+    while(h!=NULL&&h2!=NULL)
+    {
+        if(h->val < h2->val)
+        {
+            createnode(h->val);
+            h=h->next;
         }
-        else {
-            curr->next = createNode();
-            curr = curr->next;
+        else if(h2->val < h->val)
+        {
+            createnode(h2->val);
+            h2=h2->next;
         }
-
-        if(s1 != NULL && s2 != NULL) {
-            if(s1->data > s2->data) {
-                curr->data = s1->data;
-                s1 = s1->next;
-            } 
-            else if(s1->data < s2->data) {
-                curr->data = s2->data;
-                s2 = s2->next;
-            }
-            else {
-                curr->data = s2->data;
-                s1 = s1->next;
-                s2 = s2->next;
-            }
+        else
+        {
+            createnode(h->val);
+            h=h->next;
+            h2=h2->next;
         }
-        else if (s1 != NULL && s2 == NULL) {
-            curr->data = s1->data;
-            s1 = s1->next;
-        }
-        else if(s1 == NULL && s2 != NULL) {
-            curr->data = s2->data;
-            s2 = s2->next;
+        if(h3==NULL)
+            temp=h3=new;
+        else
+        {
+            temp->next=new;
+            temp=new;
         }
     }
-    return s3;
+    if(h==NULL)
+        while(h2!=NULL)
+        {
+            createnode(h2->val);
+            temp->next=new;
+            temp=new;
+            h2=h2->next;
+        }
+    else
+        while(h!=NULL)
+        {
+            createnode(h->val);
+            temp->next=new;
+            temp=new;
+            h=h->next;
+        }
+    return h3;
 }
-
-void main() {
-    Set s1 = NULL, s2 = NULL, s3 = NULL, newElement = createNode();
-    char c;
-
-    printf("Enter elements of set 1\n");
-    printf("Enter 1st  term\n");
-    do {
-        scanf("%d", &newElement->data);
-        printf("1");
-        s1 = merge(s1, newElement);
-        printf("1");
-        printf("Enter new term?[y/n]");
-        scanf("%c", &c);
-        scanf("%c", &c);
-    }while(c == 'y' || c == 'Y');
-    printf("\n\n");
-    display(s1);
-    printf("\n\n"); 
-    printf("Enter elements of set 2");
-    do {
-        scanf("%d", &newElement->data);
-        s2 = merge(s2, newElement);
-        printf("Enter new term?[y/n]");
-        scanf("%c", &c);
-        scanf("%c", &c);
-    }while(c == 'y' || c == 'Y');
-    printf("\n\n");
-    display(s2);
-    printf("\n\n");
-
-    s3 = merge(s1, s2);
-    display(s3);
+struct ele* inter(struct ele* h,struct  ele* h2)
+{
+    struct ele* h3=NULL;
+    temp = NULL;
+    while(h!=NULL&&h2!=NULL)
+    {
+        new = NULL;
+        if(h->val < h2->val)          
+            h=h->next;
+        else if(h2->val < h->val)            
+            h2=h2->next;
+        else
+        {
+            createnode(h->val);
+            h=h->next;
+            h2=h2->next;
+        }
+        if(h3==NULL)
+            temp=h3=new;
+        else
+        {
+            temp->next=new;
+            temp=new;
+        }
+    }
+    return h3;
+}
+void display(struct ele*h)
+{
+    printf("THE ELEMENTS ARE:  ");
+    while(h->next!=NULL)
+    {
+        printf("%d , ",h->val);
+        h=h->next;
+    }
+    printf("%d .",h->val);
+}
+void main()
+{
+    struct ele*head=NULL;
+    struct ele*head2=NULL;
+    int val;
+    char ch;
+    printf("enter elements of set 1\nenter first element\n");
+    do
+    {
+        printf("element:");
+        scanf("%d",&val);
+        createnode(val);
+        head=insord(head);
+        printf("enter another element(y/n)");
+        scanf("%c",&ch);
+        scanf("%c",&ch);
+    }while(ch=='y'||ch=='Y');
+    printf("enter elements of set 2\nenter first element\n");
+    do
+    {
+        printf("element:");
+        scanf("%d",&val);
+        createnode(val);
+        head2=insord(head2);
+        printf("enter another element(y/n)");
+        scanf("%c",&ch);
+        scanf("%c",&ch);
+    }while(ch=='y'||ch=='Y');
+    printf("THE TWO SETS ENTERED SUCCESSFULLY");
+    do
+    {
+       printf("\n\nmenu for operations\n1. UNION\n2. INTERSECTION\n3. DHAIRYA\n4. EXIT");
+       scanf("%d",&ch);
+       switch (ch)
+       {
+        case 1:
+           display(merge(head,head2));
+           break;
+        case 2:
+                if(inter(head,head2)== NULL)
+                    printf("no common elements...");
+                else
+                    display(inter(head,head2));
+                break;    
+        case 3:
+            printf("dhairya karega case 3");
+            break;
+        case 4:
+            printf("EXITING....");
+            break;       
+        default:
+           printf("wrong choice....enter again");
+       }
+    }while (ch!=4);
 }
